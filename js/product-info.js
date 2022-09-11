67e60b506eb2d3929819e073a4bb9f4b40dee3bb
camiloortiz89 <110353114+camiloortiz89@users.noreply.github.com>
2022-09-11 18:12:57 -0300

    todo menos el formulario - E3

--- js/product-info.js
+++ js/product-info.js
@@ -1,18 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
     getJSONData(PRODUCT_INFO_URL+localStorage.getItem('prodID')+EXT_TYPE).then(RESOLVED => {
+        const imageDefault = document.createElement('img');
         console.log(RESOLVED);
         document.getElementById('prodName').textContent = RESOLVED.data.name;
         document.getElementById('prodCost').textContent = `${RESOLVED.data.currency} ${RESOLVED.data.cost}`;
         document.getElementById('prodDescription').textContent = RESOLVED.data.description;
         document.getElementById('prodCategory').textContent = RESOLVED.data.category;
         document.getElementById('prodSoldCount').textContent = RESOLVED.data.soldCount;
-        imageByDefault(RESOLVED.data.images);
-        images(RESOLVED.data.images);
+        
+        imageDefault.classList.add('img-thumbnail', 'shadow-sm');
+        imageDefault.setAttribute('src', RESOLVED.data.images[0]);
+        document.getElementById('largeImg').appendChild(imageDefault);
+
+        showImages(RESOLVED.data.images);
+        showProdsRelated(RESOLVED.data.relatedProducts);
+    });
+
+    getJSONData(PRODUCT_INFO_COMMENTS_URL+localStorage.getItem('prodID')+EXT_TYPE).then(RESOLVED => {
+        console.log(RESOLVED);
+        showProdComments(RESOLVED.data);
     });
 });
 
 
-function images(images){
+function showImages(images){
     for (const elem of images) {
         let imgs = document.createElement('img');
         imgs.setAttribute('src', elem);
@@ -27,10 +38,46 @@ function images(images){
     }
 }
 
-function imageByDefault(image){
-    const imageDefault = document.createElement('img');
-    imageDefault.classList.add('img-thumbnail', 'shadow-sm');
-    imageDefault.setAttribute('src', image[0]);
+function showProdsRelated(related){
+    for (const elem of related) {
+        let prodRel = document.createElement('div');
+        prodRel.classList.add('list-group-item', 'list-group-item-action', 'w-25', 'ms-3', 'border', 'm-2');
+        prodRel.innerHTML = `
+            <img src="${elem.image}" class="img-thumbnail">
+            <p class="h4">${elem.name}</p>
+        `
+        document.getElementById('prodList').appendChild(prodRel);
 
-    document.getElementById('largeImg').appendChild(imageDefault);
+        prodRel.addEventListener('click', () =>{
+            localStorage.setItem('prodID', elem.id);
+            window.location.reload();
+        });
+    }
+}
+
+function showProdComments(comments) {
+    for (const elem of comments) {
+        let prodCom = document.createElement('div');
+        prodCom.classList.add('pb-2', 'list-group-item');
+        prodCom.innerHTML = `
+            <p class="fw-bold">${elem.user} - 
+            <span class="small text-muted">${elem.dateTime}</span>
+            <span class="float-end">${showScore(elem.score)}</span>
+            </p>
+            <p>${elem.description}</p>
+        `
+        document.getElementById('prodComments').appendChild(prodCom);
+    }
+}
+
+function showScore(num){
+    let stars = '';
+    for (let i = 0; i < 5; i++) {
+        if (i < num) {
+            stars += `<span class="fa fa-star checked"></span>`;
+        }else{
+            stars += `<span class="fa fa-star"></span>`;
+        }
+    }
+    return stars;
 }
